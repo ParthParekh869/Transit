4102e43b99c9cc72c439d7731b8e4313edbeb67f
Parth <parekhpr@myumanitoba.ca>
2025-09-02 02:39:37 +0530

    Initial Commit

---
+++ TransitTests/TransitTests.swift
@@ -0,0 +1,17 @@
+//
+//  TransitTests.swift
+//  TransitTests
+//
+//  Created by Parth Parekh on 2025-09-02.
+//
+
+import Testing
+@testable import Transit
+
+struct TransitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
